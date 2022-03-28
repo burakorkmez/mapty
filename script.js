645b811c1c35bf749c358c7697c50064168188e6
Burak Örkmez <burakorkmezz@gmail.com>
2022-03-28 19:10:32 +0300

    Classes for workout data created

--- script.js
+++ script.js
@@ -11,17 +11,57 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map;
-let mapEvent;
+class Workout {
+	date = new Date();
+	id = Date.now().toString().slice(-10);
 
+	constructor(coords, distance, duration) {
+		this.coords = coords; // [lat,lng]
+		this.distance = distance; // in km
+		this.duration = duration; // in min
+	}
+}
+
+class Running extends Workout {
+	constructor(coords, distance, duration, cadence) {
+		super(coords, distance, duration);
+		this.cadence = cadence;
+		this.calcPace();
+	}
+
+	calcPace() {
+		// min/km
+		this.pace = this.duration / this.distance;
+		return this.pace;
+	}
+}
+
+class Cycling extends Workout {
+	constructor(coords, distance, duration, elevationGain) {
+		super(coords, distance, duration);
+		this.elevationGain = elevationGain;
+		this.calcSpeed();
+	}
+
+	calcSpeed() {
+		// km/h
+		this.speed = this.distance / (this.duration / 60);
+		return this.speed;
+	}
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1);
+
+// *****************************************
+// APPLICATION
 class App {
 	#map;
 	#mapEvent;
 	constructor() {
 		this._getPosition();
-
 		form.addEventListener('submit', this._newWorkout.bind(this));
-
 		inputType.addEventListener('change', this._toggleElevationField);
 	}
 
@@ -35,7 +75,6 @@ class App {
 	_loadMap(position) {
 		const { latitude } = position.coords;
 		const { longitude } = position.coords;
-		console.log(`https://www.google.com/maps/@${latitude},${longitude},8.25z`);
 		const coords = [latitude, longitude];
 		this.#map = L.map('map').setView(coords, 13);
 
